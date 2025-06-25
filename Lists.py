03e459b7b34ecc27fb8fab35e116a89bece79e43
Yasir-Saad-Zuhair <9801002@gmail.com>
2025-06-25 23:34:10 +0600

    Python

---
+++ Lists.py
@@ -0,0 +1,16 @@
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# print(len(thislist))
+
+# list1 = ["abc", 34, True, 40, "male"]
+
+# mylist = ["apple", "banana", "cherry"]
+# print(type(mylist))
+
+# thislist = list(("apple", "banana", "cherry")) 
+# print(thislist)
