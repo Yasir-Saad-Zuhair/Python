e7da4b29bf5a9d5dd399777db44ebd810e604526
Yasir-Saad-Zuhair <9801002@gmail.com>
2025-06-29 18:50:18 +0600

    Test

---
+++ math.py
@@ -0,0 +1,496 @@
+# Import math Library
+import math
+
+# Return the arc cosine of numbers
+print(math.acos(0.55))
+print(math.acos(-0.55))
+print(math.acos(0))
+print(math.acos(1))
+print(math.acos(-1))
+
+# Import math Library
+import math
+
+# Return the inverse hyperbolic cosine of different numbers
+print (math.acosh(7))
+print (math.acosh(56))
+print (math.acosh(2.45))
+print (math.acosh(1))
+
+# Import math Library
+import math
+
+# Return the arc sine of numbers
+print(math.asin(0.55))
+print(math.asin(-0.55))
+print(math.asin(0))
+print(math.asin(1))
+print(math.asin(-1))
+
+# Import math Library
+import math
+
+# Return the arc sine of numbers
+print(math.asin(0.55))
+print(math.asin(-0.55))
+print(math.asin(0))
+print(math.asin(1))
+print(math.asin(-1))
+
+# Import math Library
+import math
+
+# Return the inverse hyperbolic sine of numbers
+print(math.asinh(7))
+print(math.asinh(56))
+print(math.asinh(2.45))
+print(math.asinh(1))
+
+#Import math Library
+import math
+
+#fReturn the arc tangent of different numbers
+print (math.atan(0.39))
+print (math.atan(67))
+print (math.atan(-21))
+
+# Import math Library
+import math
+
+# Return the arc tangent of y/x in radians
+print(math.atan2(8, 5))
+print(math.atan2(20, 10))
+print(math.atan2(34, -7))
+
+#Import math Library
+import math
+
+#print the hyperbolic arctangent of different numbers
+print(math.atanh(0.59))
+print(math.atanh(-0.12))
+
+# Import math library
+import math
+
+# Round a number upward to its nearest integer
+print(math.ceil(1.4))
+print(math.ceil(5.3))
+print(math.ceil(-5.3))
+print(math.ceil(22.6))
+print(math.ceil(10.0))
+
+# Import math Library
+import math
+
+# Initialize the number of items to choose from
+n = 7
+
+# Initialize the number of possibilities to choose
+k = 5
+
+# Print total number of possible combinations
+print (math.comb(n, k))
+
+#Import math Library
+import math
+
+#Return the value of the first parameter and the sign of the second parameter
+print(math.copysign(4, -1))
+print(math.copysign(-8, 97.21))
+print(math.copysign(-43, -76))
+
+# Import math Library
+import math
+
+# Return the cosine of different numbers
+print (math.cos(0.00))
+print (math.cos(-1.23))
+print (math.cos(10))
+print (math.cos(3.14159265359))
+
+# Import math Library
+import math
+
+# Return the hyperbolic cosine of different numbers
+print (math.cosh(1))
+print (math.cosh(8.90))
+print (math.cosh(0))
+print (math.cosh(1.52))
+
+# Import math Library
+import math
+
+# Convert from radians to degrees:
+print (math.degrees(8.90))
+print (math.degrees(-20))
+print (math.degrees(1))
+print (math.degrees(90))
+
+# Import math Library
+import math
+
+p = [3]
+q = [1]
+
+# Calculate Euclidean distance
+print (math.dist(p, q))
+
+p = [3, 3]
+q = [6, 12]
+
+# Calculate Euclidean distance
+print (math.dist(p, q))
+
+# Import math Library
+import math
+
+# Print error function for different numbers
+print (math.erf(0.67))
+print (math.erf(1.34))
+print (math.erf(-6))
+
+# Import math Library
+import math
+
+# Print complementary error function for different numbers
+print (math.erfc(0.67))
+print (math.erfc(1.34))
+print (math.erfc(-6))
+
+#Import math Library
+import math
+
+#find the exponential of the specified value
+print(math.exp(65))
+print(math.exp(-6.89))
+
+#Import math Library
+import math
+
+#Return the exponential value of a number - 1
+print(math.expm1(32))
+print(math.expm1(-10.89))
+
+#Import math Library
+import math
+
+#Print absolute values from numbers
+print(math.fabs(-66.43))
+print(math.fabs(-7))
+
+#Import math Library
+import math
+
+#Return factorial of a number
+print(math.factorial(9))
+print(math.factorial(6))
+print(math.factorial(12))
+
+# Import math library
+import math
+
+# Round numbers down to the nearest integer
+print(math.floor(0.6))
+print(math.floor(1.4))
+print(math.floor(5.3))
+print(math.floor(-5.3))
+print(math.floor(22.6))
+print(math.floor(10.0))
+
+# Import math Library
+import math
+
+# Return the remainder of x/y
+print(math.fmod(20, 4))
+print(math.fmod(20, 3))
+print(math.fmod(15, 6))
+print(math.fmod(-10, 3))
+print(math.fmod(0, 0))
+
+#Import math Library
+import math
+
+#Return mantissa and exponent of numbers
+print(math.frexp(4))
+print(math.frexp(-4))
+print(math.frexp(7))
+
+# Import math Library
+import math
+
+# Print the sum of all items
+print(math.fsum([1, 2, 3, 4, 5]))
+print(math.fsum([100, 400, 340, 500]))
+print(math.fsum([1.7, 0.3, 1.5, 4.5]))
+
+# Import math Library
+import math
+
+# Return the gamma function for different numbers
+print(math.gamma(-0.1))
+print(math.gamma(8))
+print(math.gamma(1.2))
+print(math.gamma(80))
+print(math.gamma(-0.55))
+
+#Import math Library
+import math
+
+#find the  the greatest common divisor of the two integers
+print (math.gcd(3, 6))
+print (math.gcd(6, 12))
+print (math.gcd(12, 36))
+print (math.gcd(-12, -36))
+print (math.gcd(5, 12))
+print (math.gcd(10, 0))
+print (math.gcd(0, 34))
+print (math.gcd(0, 0))
+
+#Import math Library
+import math
+
+#set perpendicular and base
+perpendicular = 10
+base = 5
+
+#print the hypotenuse of a right-angled triangle
+print(math.hypot(parendicular, base))
+
+#Import math Library
+import math
+
+#compare the closeness of two values
+print(math.isclose(1.233, 1.4566))
+print(math.isclose(1.233, 1.233))
+print(math.isclose(1.233, 1.24))
+print(math.isclose(1.233, 1.233000001))
+
+# Import math Library
+import math
+
+# Check whether the values are finite or not
+print(math.isfinite(2000))
+print(math.isfinite(-45.34))
+print(math.isfinite(+45.34))
+print(math.isfinite(math.inf))
+print(math.isfinite(float("nan")))
+print(math.isfinite(float("inf")))
+print(math.isfinite(float("-inf")))
+print(math.isfinite(-math.inf))
+print(math.isfinite(0.0))
+
+# Import math Library
+import math
+
+# Check whether the values are infinite or not
+print(math.isinf(56))
+print(math.isinf(-45.34))
+print(math.isinf(+45.34))
+print(math.isinf(math.inf))
+print(math.isinf(float("nan")))
+print(math.isinf(float("inf")))
+print(math.isinf(float("-inf")))
+print(math.isinf(-math.inf))
+
+# Import math Library
+import math
+
+# Check whether some values are NaN or not
+print (math.isnan (56))
+print (math.isnan (-45.34))
+print (math.isnan (+45.34))
+print (math.isnan (math.inf))
+print (math.isnan (float("nan")))
+print (math.isnan (float("inf")))
+print (math.isnan (float("-inf")))
+print (math.isnan (math.nan))
+
+# Import math Library
+import math
+
+# Print the square root of different numbers
+print (math.sqrt(10))
+print (math.sqrt (12))
+print (math.sqrt (68))
+print (math.sqrt (100))
+
+# Round square root downward to the nearest integer
+print (math.isqrt(10))
+print (math.isqrt (12))
+print (math.isqrt (68))
+print (math.isqrt (100))
+
+#Import math Library
+import math
+
+#Return value of x * (2**i)
+print(math.ldexp(9, 3))
+print(math.ldexp(-5, 2))
+print(math.ldexp(15, 2))
+
+# Import math Library
+import math
+
+# Return the log gamma value of different numbers
+print (math.lgamma(7))
+print (math.lgamma(-4.2))
+
+# Import math Library
+import math
+
+# Return the natural logarithm of different numbers
+print(math.log(2.7183))
+print(math.log(2))
+print(math.log(1))
+
+# Import math Library
+import math
+
+# Return the base-10 logarithm of different numbers
+print(math.log10(2.7183))
+print(math.log10(2))
+print(math.log10(1))
+
+# Import math Library
+import math
+
+# Return the log(1+number) for different numbers
+print(math.log1p(2.7183))
+print(math.log1p(2))
+print(math.log1p(1))
+
+# Import math Library
+import math
+
+# Return the base-2 logarithm of different numbers
+print(math.log2(2.7183))
+print(math.log2(2))
+print(math.log2(1))
+
+# Import math Library
+import math
+
+# Initialize the number of items to choose from
+n = 7
+
+# Initialize the number of items to choose
+k = 5
+
+# Print the number of ways to choose k items from n items
+print (math.perm(n, k))
+
+# Import math Library
+import math
+
+#Return the value of 9 raised to the power of 3
+print(math.pow(9, 3))
+
+# Import math Library
+import math
+
+sequence = (2, 2, 2)
+
+#Return the product of the elements
+print(math.prod(sequence))
+
+# Import math Library
+import math
+
+# Convert different degrees into radians
+print(math.radians(180))
+print(math.radians(100.03))
+print(math.radians(-20))
+
+# Import math Library
+import math
+
+# Return the remainder of x/y
+print (math.remainder(9, 2))
+print (math.remainder(9, 3))
+print (math.remainder(18, 4))
+
+# Import math Library
+import math
+
+# Return the sine of different values
+print (math.sin(0.00))
+print (math.sin(-1.23))
+print (math.sin(10))
+print (math.sin(math.pi))
+print (math.sin(math.pi/2))
+
+# Import math Library
+import math
+
+# Return the hyperbolic sine of different values
+print(math.sinh(0.00))
+print(math.sinh(-23.45))
+print(math.sinh(23))
+print(math.sinh(1.00))
+print(math.sinh(math.pi))
+
+# Import math Library
+import math
+
+# Return the square root of different numbers
+print (math.sqrt(9))
+print (math.sqrt(25))
+print (math.sqrt(16))
+
+# Import math Library
+import math
+
+# Return the tangent of different numbers
+print (math.tan(90))
+print (math.tan(-90))
+print (math.tan(45))
+print (math.tan(60))
+
+# Import math Library
+import math
+
+# Return the hyperbolic tangent of different numbers
+print(math.tanh(8))
+print(math.tanh(1))
+print(math.tanh(-6.2))
+
+# Import math Library
+import math
+
+# Return the truncated integer parts of different numbers
+print(math.trunc(2.77))
+print(math.trunc(8.32))
+print(math.trunc(-99.29))
+
+# Import math Library
+import math
+
+# Print the value of E
+print (math.e)
+
+# Import math Library
+import math
+
+# Print the positive infinity
+print (math.inf)
+
+# Print the negative infinity
+print (-math.inf)
+
+# Import math Library
+import math
+
+# Print the value of nan
+print (math.nan)
+
+# Import math Library
+import math
+
+# Print the value of pi
+print (math.pi)
+
+# Import math Library
+import math
+
+# Print the value of tau
+print (math.tau)
