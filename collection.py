51ec5e723c3e4f2ee210e2b12bb45d5c19bfb2bd
Yasir-Saad-Zuhair <9801002@gmail.com>
2025-06-29 18:52:58 +0600

    Test

---
+++ collection.py
@@ -0,0 +1,159 @@
+# # thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+# # print(thislist)
+# list1 = ("apple","Banana", "cherry")
+# print(type(list1))
+# thislist = ["apple","Banana", "cherry"]
+# print(type(thislist))
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[0:6])
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[-4:-1])
+# thislist = ["apple", "banana", "cherry"]
+# if "Murgi" in thislist:
+#   print("Yes, 'apple' is in the fruits list")
+# else:
+#   print("Murgi, is not in this list")
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist[1:2] = ["blackcurrant", "carrot"]
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:3] = ["watermelon"]
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(2, "watermelon")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[1])
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[1])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:5])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[:4])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[-4:-1])
+
+# thislist = ["apple", "banana", "cherry"]
+# if "apple" in thislist:
+#   print("Yes, 'apple' is in the fruits list")
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+# thislist.remove("banana")
+# print(thislist) 
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop(1)
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop()
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist[0]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.clear()
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(1, "orange")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thistuple = ("kiwi", "orange")
+# thislist.extend(thistuple)
+# print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+    print(thislist[i])
+    i = i + 1
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+fruits = ['apple', 'banana', 'cherry']
+fruits.append("orange")
+
+fruits = ['apple', 'banana', 'cherry', 'orange']
+
+fruits.clear()
+
+fruits = ['apple', 'banana', 'cherry', 'orange']
+
+x = fruits.copy()
+
+fruits = ['apple', 'banana', 'cherry']
+
+x = fruits.count("cherry")
+
+fruits = ['apple', 'banana', 'cherry']
+
+cars = ['Ford', 'BMW', 'Volvo']
+
+fruits.extend(cars)
+
+fruits = ['apple', 'banana', 'cherry']
+
+x = fruits.index("cherry")
+
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.insert(1, "orange")
+
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.pop(1)
+
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.remove("banana")
+
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.reverse()
+
+cars = ['Ford', 'BMW', 'Volvo']
+
+cars.sort()
+
+
